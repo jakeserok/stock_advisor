4df65025321650e68cc49a2ca1c326e628ac9250
Jake Serok <jakeserok@Jakes-MBP.lan>
2023-05-03 20:40:47 -0400

    Migrate to sidekiq

--- app/models/ai_response.rb
+++ app/models/ai_response.rb
@@ -1,3 +1,17 @@
 class AiResponse < ApplicationRecord
   validates_presence_of :stock
+
+  def self.search(stock)
+    stock = self.find_by(stock: stock)
+    stock_symbols = YAML.load_file('config/stocks.yml')['symbols']
+    return if stock_symbols.include?(stock)
+
+    trends = FinanceAiAdvisor.new(stock).trends
+    return if trends.blank?
+
+    temp = stock_symbols
+    temp['symbols'] << stock
+    File.open('config/stocks.yml', 'w') { |file| file.write(temp.to_yaml) }
+    stock
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+require 'sidekiq/web'
 Rails.application.routes.draw do
   devise_for :users
   root "stock_info#index"
   get 'stocks', to: 'stock_info#stocks'
   get 'stock', to: 'stock_info#stock'
+  mount Sidekiq::Web => "/sidekiq"
 end
